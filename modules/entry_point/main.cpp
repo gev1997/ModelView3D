ac4e4f09394905c8d39b3c423c4187dad71f7922
Gevorg <qamalyan86@gmail.com>
2025-12-20 00:40:05 +0400

    Pass model path as cmd line argument

--- modules/entry_point/main.cpp
+++ modules/entry_point/main.cpp
@@ -5,14 +5,17 @@
 
 // std
 
-int main()
+int main(int argc, char* argv[])
 {
+	if (argc < 2)
+		return 0;
+	
 	gui::model_view_3D_app app;
 
 	if (!app.init())
 		return -1;
 
-	if (!app.load_model("D:\\Project\\ModelView3D\\models\\eiffel.stl"))
+	if (!app.load_model(argv[1]))
 		return -1;
 
 	app.main_loop();
