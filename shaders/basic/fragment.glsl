8e3939c2dc015a4cb0ee9391774044dd09c13407
Gevorg <qamalyan86@gmail.com>
2025-12-21 16:57:19 +0400

    Keep the lighting in a fixed position

--- shaders/basic/fragment.glsl
+++ shaders/basic/fragment.glsl
@@ -3,20 +3,26 @@ out vec4 FragColor;
 
 in vec3 Normal;
 in vec3 FragPos;
-uniform vec3 lightPos;
-uniform vec3 viewPos;
+uniform vec3 lightPos;   // now expected in view space
+uniform vec3 viewPos;    // camera position in view space, usually (0,0,0)
 uniform vec3 objectColor;
+uniform mat4 view;
 
 void main()
 {
+    // Transform fragment position to view space
+    vec3 FragPosView = vec3(view * vec4(FragPos, 1.0));
+
+    // Transform normal to view space
+    vec3 NormalView = normalize(mat3(transpose(inverse(view))) * Normal);
+
     // Ambient
     float ambientStrength = 0.2;
     vec3 ambient = ambientStrength * objectColor;
 
     // Diffuse
-    vec3 norm = normalize(Normal);
-    vec3 lightDir = normalize(lightPos - FragPos);
-    float diff = max(dot(norm, lightDir), 0.0);
+    vec3 lightDir = normalize(lightPos - FragPosView); // lightPos in view space
+    float diff = max(dot(NormalView, lightDir), 0.0);
     vec3 diffuse = diff * objectColor;
 
     vec3 result = ambient + diffuse;
